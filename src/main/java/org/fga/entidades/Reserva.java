c323b42f42768f39c5e6fca41b935d31f6b1fa3e
gustav <gustavuh0012@gmail.com>
2025-06-21 17:19:40 -0300

    verificar disponibilidade melhorada e alterações nos atributos de EspacosFisicos

--- src/main/java/org/fga/entidades/Reserva.java
+++ src/main/java/org/fga/entidades/Reserva.java
@@ -24,4 +24,5 @@ public class Reserva {
         return sobreposicaoDeHoras;
     }
 
+
 }
--- src/main/java/org/fga/espacos/EspacosFisicos.java
+++ src/main/java/org/fga/espacos/EspacosFisicos.java
@@ -2,31 +2,56 @@ package org.fga.espacos;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.fga.entidades.Reserva;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 @Getter
 @Setter
 
 public abstract class EspacosFisicos {
+    private String nomeEspaco;
     private int capacidade;
     private String loc;
-    private List<String> disp;
     private List<String> equipamentos;
+    private List<Reserva> reservasDoEspaco;
+    private List<Reserva> historico;
+
     Scanner sc = new Scanner(System.in);
-    public EspacosFisicos(int capacidade, String loc, List<String> disp, List<String> equipamentos) {
+    public EspacosFisicos(String nomeEspaco ,int capacidade, String loc, List<String> disp, List<String> equipamentos) {
+        this.nomeEspaco = nomeEspaco;
         this.capacidade = capacidade;
         this.loc = loc;
-        this.disp = disp;
-        this.equipamentos = equipamentos;
+        this.reservasDoEspaco = new ArrayList<>();
+        this.equipamentos = new ArrayList<>();
+        this.historico = new ArrayList<>();
+    }
+
+    public boolean verificarDisponibilidade(Reserva novaReserva) {
+        for (Reserva reservasDoEspaco : getReservasDoEspaco()) {
+            if (Reserva.sobreposicao(reservasDoEspaco, novaReserva)) {
+                System.out.println("Reserva não concedida!");
+                return false;
+            }
+        }
+        System.out.println("Reserva Concedida!");
+        getReservasDoEspaco().add(novaReserva);
+        getHistorico().add(novaReserva);
+        return true;
     }
 
 
 
-    public void InformacaoEspaco() {
+    public void InformacaoEspaco(EspacosFisicos espacos) {
+        System.out.println("Nome do Espaco: " + getNomeEspaco());
         System.out.println("Capacidade: " + getCapacidade());
         System.out.println("Localizacao: " + getLoc());
-        System.out.println("Disponibilidade: " + getDisp());
         System.out.println("Equipamentos: " + getEquipamentos());
+        System.out.println("Reservas do Espaco: ");
+        for (Reserva reservasDoEspaco : getReservasDoEspaco()) {
+            System.out.println(reservasDoEspaco);
+        }
     }
 
     }
